f1b668fe623b41c251a069993718ea3bcb0cb467
Arundevloper <arunsharma96025@gmail.com>
2024-03-30 18:02:18 +0530

    dfd

--- Backend/middleware/authenticationToken.middleware.js
+++ Backend/middleware/authenticationToken.middleware.js
@@ -7,7 +7,7 @@ function authenticateToken(req, res, next) {
     
     if (!token) {
         
-        return res.status(401).json({ error: "Unauthorized" });
+        return res.status(401).json({ error: "Unauthorized",token });
     }
     else{
         
