64f7fcf692e6c58bb42d32e4a0290e21f214a7f2
Arundevloper <arunsharma96025@gmail.com>
2024-03-30 17:37:16 +0530

    dfd

--- Backend/controllers/login.controller.js
+++ Backend/controllers/login.controller.js
@@ -4,7 +4,38 @@ const bcrypt = require('bcrypt');
 
 async function login(req, res) {
     const { email, password } = req.body;
-        res.status(200).json("2000");
+
+    try {
+        // Find the user by email
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ error: "Invalid username or password" });
+        }
+
+        // Compare the provided password with the hashed password
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(400).json({ error: "Invalid username or password" });
+        }
+
+        console.log("Login successfully");
+        const token = setUser(user);
+
+
+
+
+        // If login is successful, send JSON response indicating success
+        res.status(200).json({
+            token
+        });
+
+
+    } catch (error) {
+        console.error("Login error:", error.message);
+        return res.status(500).json({ error: "Internal server error" });
+    }
 }
 
 module.exports = login;
