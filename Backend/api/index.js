423aaa028ca77bd70655d81f2f4dc59a54d1d7fd
Arundevloper <arunsharma96025@gmail.com>
2024-03-30 15:28:33 +0530

    in

---
+++ Backend/api/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const path = require('path');
+const cors = require('cors');
+const connectDb = require('./database/dbConnect');
+const Routes = require('./Routes/auth.Routes');
+const pdfRoutes = require('./Routes/pdf.Routes');
+
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true 
+}));
+
+
+app.get("/api/" ,(req,res)=>{
+  res.json("hello World 123");
+})
+
+app.get("/api/login" ,(req,res)=>{
+  res.json("hello World 123");
+})
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/', Routes);
+app.use('/', pdfRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+connectDb();
+
+
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+});
