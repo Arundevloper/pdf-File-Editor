7fa5c3f071516ae52824c1f469c8aa3b6b162a80
Arundevloper <arunsharma96025@gmail.com>
2024-03-30 22:09:56 +0530

    sd

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -7,6 +7,14 @@ const connectDb = require('../database/dbConnect');
 const Routes = require('../Routes/auth.Routes');
 const pdfRoutes = require('../Routes/pdf.Routes');
 
+app.use(
+  cors({
+    origin: ["https://pdf-file-editor.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 
 app.get("/api/" ,(req,res)=>{
   res.json("hello World 1234");
@@ -18,6 +26,11 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/', Routes);
@@ -32,13 +45,3 @@ app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
 });
 
-app.use(
-  cors({
-    origin: ["https://pdf-file-editor.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
